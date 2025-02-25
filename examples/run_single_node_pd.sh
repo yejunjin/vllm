8b6cb68831ce46bcef0f037100c92838de704a39
yejunjin <jinyejun.jyj@alibaba-inc.com>
2025-02-25 16:11:04 +0800

    add qwen2.5-270ba37b-instruct support

--- examples/run_single_node_pd.sh
+++ examples/run_single_node_pd.sh
@@ -6,5 +6,9 @@ export VLLM_USE_MODELSCOPE=True
 
 # control num_gpu_blocks_override to avoid miscomputing avaiable gpu blocks
 # since we have two engines now!
-vllm serve Qwen/Qwen2.5-0.5B-Instruct -tp 2 \
+vllm serve /home/admin/resource/model/16394f66.qwen2.5-270ba37b-instruct/128k-20250112/ -tp 8 \
+    --gpu_memory_utilization 0.9 \
     --num_gpu_blocks_override 4096
+
+# Qwen/Qwen2.5-0.5B-Instruct
+#/home/admin/resource/model/16394f66.qwen2.5-270ba37b-instruct/128k-20250112/
